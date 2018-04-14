329307f40c3fc4b9e2c5968aea36156e95ad6dee
orakki <amanishwar@outlook.com>
2018-04-14 15:08:55 +0530

    Great new user,and alert others

--- server/server.js
+++ server/server.js
@@ -14,16 +14,30 @@ app.use(express.static(publicPath));
 io.on('connection',(socket)=>{
   console.log('new user connected');
 
-
+socket.emit('newMessage',{
+  from:'Admin',
+  text: 'welcome to the chat app'
+});
+socket.broadcast.emit('newMessage',{
+  from:'Admin',
+  text:'new user joined',
+  createAt: new Date().getTime()
+});
 
   socket.on('createMessage',(message)=>{
     console.log('createMessage',message);
     io.emit('newMessage',{
-      from:message.from,
-      text:message.text,
-      createAt:new Date().getTime()
-    });
+    from:message.from,
+    text:message.text,
+    createAt:new Date().getTime()
   });
+
+  // socket.broadcast.emit('newMessage',{
+  //   from:message.from,
+  //   text:message.text,
+  //   createAt:new Date().getTime()
+  // });
+});
 
   socket.on('disconnect',()=>{
     console.log('user was disconnected');
