370e768dd9159b33148f72b51345a97b482061b9
orakki <amanishwar@outlook.com>
2018-04-14 13:56:36 +0530

    Emit newMessage onn createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -3,11 +3,6 @@ var socket = io();
 
 socket.on('connect',function(){
   console.log('connected to server');
-
-    socket.emit('createMessage',{
-     to:'man.gun.tappu@gmail.com',
-     text:'hey. this is aman'
-   });
 });
 
 
