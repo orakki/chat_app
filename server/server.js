22ee23c3041fa066dfb910bdced9122fafce0db9
orakki <amanishwar@outlook.com>
2018-04-15 13:40:40 +0530

    add form and show incoming message

--- server/server.js
+++ server/server.js
@@ -19,9 +19,10 @@ io.on('connection',(socket)=>{
 socket.emit('newMessage',generateMessage('Admin','welcome to chat app!!!'));
 socket.broadcast.emit('newMessage',generateMessage('Admin','new user join'));
 
-  socket.on('createMessage',(message)=>{
+  socket.on('createMessage',(message, callback)=>{
     console.log('createMessage',message);
     io.emit('newMessage',generateMessage(message.from,message.text));
+    callback('This is from the server');
 
   // socket.broadcast.emit('newMessage',{
   //   from:message.from,
