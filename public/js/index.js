43a8ffa7f4a38d426fc65b666ed01a1b3eef1c59
orakki <amanishwar@outlook.com>
2018-04-16 22:24:39 +0530

    auto scroll is user is near to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,20 @@
 var socket = io();
 
+function scrollTopBottom() {
+  //selector
+  var messages = jQuery('#messages');
+  var newMessage = messages.children('li:last-child');
+  //height
+  var clientHeight = messages.prop('clientHeight');
+  var scrollTop = messages.prop('scrollTop');
+  var scrollHeight = messages.prop('scrollHeight');
+  var newMessageHeight = newMessage.innerHeight();
+  var lastMessageHeight = newMessage.prev().innerHeight();
+
+  if(clientHeight + scrollTop + newMessageHeight + lastMessageHeight>= scrollHeight){
+    messages.scrollTop(scrollHeight);
+  }
+}
 
 socket.on('connect',function(){
   console.log('connected to server');
@@ -19,7 +34,7 @@ socket.on('newMessage',function(message) {
     createdAt: formattedTime
   });
   jQuery('#messages').append(html);
-
+scrollTopBottom();
 
   // var li = jQuery('<li></li>');
   // li.text(`${message.from} ${formattedTime}: ${message.text}`);
@@ -37,6 +52,7 @@ var html = Mustache.render(template,{
 });
 
 jQuery('#messages').append(html);
+scrollTopBottom();
 
 // var li = jQuery('<li></li>');
 // var a = jQuery('<a target = "_blank">My current location</a>');
